c331647f26b10d23204fd86164d4692937e00cb8
FaizAhamed2o1 <faizahmed23014@gmail.com>
2024-10-28 00:53:12 +0600

    nested routing added. Doesn't change header and footer but changes the content based on he nav link that's been clicked

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,16 @@
+const Users = () => {
+  return (
+    <div>
+      <h2>Our Users</h2>
+      <ul>
+        <li>Faiz</li>
+        <li>Asha</li>
+        <li>Suraiya</li>
+        <li>Labiba</li>
+        <li>Ayman</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
