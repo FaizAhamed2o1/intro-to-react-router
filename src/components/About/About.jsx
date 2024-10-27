f0f48501d66d65f55151a82de5205493723605df
FaizAhamed2o1 <faizahmed23014@gmail.com>
2024-10-28 00:33:38 +0600

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit corporis
+        voluptatibus ut earum nam porro accusantium delectus assumenda neque
+        quasi magnam doloremque fugit exercitationem et, repellendus impedit quo
+        aliquid nihil!
+      </p>
+    </div>
+  );
+};
+
+export default About;
